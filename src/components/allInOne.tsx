4641409f93348c5713ccb8357bb10b8095905429
wagihmohamed <wagihm969@gmail.com>
2022-07-25 01:21:11 +0200

    added all in one icons

--- src/components/allInOne.tsx
+++ src/components/allInOne.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon } from "@heroicons/react/outline";
+import { AdjustmentsIcon, BellIcon, BookOpenIcon, BriefcaseIcon, CheckIcon, PaperAirplaneIcon, PhoneIcon } from "@heroicons/react/outline";
 import { AllInOneCard } from "./sharedComponents/allInOneCard";
 interface AllInOneDataTypes {
     title: string;
@@ -10,34 +10,34 @@ export const AllInOne = (): JSX.Element => {
 
     const allInOneData: AllInOneDataTypes[] = [
         {
-            title: 'Documentation',
+            title: 'Notifications',
             body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
+            icon: <BellIcon className="w-7 mr-4 text-green-600" />
+        },
+        {
+            title: 'Support',
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
+            icon: <PhoneIcon className="w-7 mr-4 text-green-600" />
+        },
+        {
+            title: 'Strong API',
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
+            icon: <PaperAirplaneIcon className="w-7 mr-4 text-green-600" />
+        },
+        {
+            title: 'Maintenance',
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
+            icon: <AdjustmentsIcon className="w-7 mr-4 text-green-600" />
         },
         {
             title: 'Documentation',
             body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
+            icon: <BookOpenIcon className="w-7 mr-4 text-green-600" />
         },
         {
-            title: 'Documentation',
+            title: 'Global Buissness',
             body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
-        },
-        {
-            title: 'Documentation',
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
-        },
-        {
-            title: 'Documentation',
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
-        },
-        {
-            title: 'Documentation',
-            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At suscipit blanditiis est! Amet laboriosam ab cum porro nobis. Expedita, excepturi quam ratione distinctio dolor ipsum omnis facere aut nam voluptatibus?",
-            icon: <CheckIcon className="w-7 mr-4 text-green-600" />
+            icon: <BriefcaseIcon className="w-7 mr-4 text-green-600" />
         },
     ]
 
@@ -46,7 +46,7 @@ export const AllInOne = (): JSX.Element => {
             <div className="max-w-7xl mx-auto px-2 text-center">
                 <h2 className="font-bold text-6xl ">All-In-One Platform</h2>
                 <p className="font-normal mt-6 px-16">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure temporibus iusto labore earum. Nam, veniam? Nesciunt officia consequatur cupiditate. Vero doloribus expedita quis voluptatum repudiandae provident facilis ab in perferendis?</p>
-                <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 p-16 text-start">
+                <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 p-16 gap-6 text-start">
                     <AllInOneCard
                         dataArray={allInOneData}
                     />
